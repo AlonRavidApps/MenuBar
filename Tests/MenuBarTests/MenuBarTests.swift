1b81907d702d1ff2e093d66d62ebab385b533f68
Alon Ravid <alonri75@walla.com>
2021-07-25 16:41:32 +0300

    Initial Commit

--- Tests/MenuBarTests/MenuBarTests.swift
+++ Tests/MenuBarTests/MenuBarTests.swift
@@ -6,6 +6,6 @@
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
             // results.
-            XCTAssertEqual(MenuBar().text, "Hello, World!")
+        //    XCTAssertEqual(MenuBar().text, "Hello, World!")
         }
     }
